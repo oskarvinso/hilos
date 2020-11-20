6ec20eab4a51bc8878f58a8f6449cc709c77a94d
oskarvinso <oxcarvincent@gmail.com>
2020-11-19 22:33:52 -0500

    Update Proceso1.java
    recorte el conteo para visualizar mejor

--- Proceso1.java
+++ Proceso1.java
@@ -3,7 +3,7 @@ public class Proceso1 extends Thread{
 
     @Override
     public void run(){
-        for (int i = 0; i <= 99; i++){
+        for (int i = 0; i <= 10; i++){
             System.out.println("Numero par " + num_par);
             num_par = num_par + 2;
         }
