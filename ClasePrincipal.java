2925d2639a7c52cfe6a09edd91ec4715ef84a770
oskarvinso <oxcarvincent@gmail.com>
2020-11-23 19:18:14 -0500

    Delete ClasePrincipal.java

--- ClasePrincipal.java
+++
@@ -1,8 +0,0 @@
-public class ClasePrincipal {
-    public static void main(String[] args) {
-        Proceso1 hilo1 = new Proceso1();
-        Thread hilo2 = new Thread(new Proceso2());
-        hilo1.start();
-        hilo2.start();
-    }
-}
